48dab340413dfaeecb8cf5f72cb486a835c0c6bd
Zain Ali <19750152+zainali-xyz@users.noreply.github.com>
2021-11-12 14:14:29 -0500

    Fixed dependency issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import discord
-from discord.ext import commands, context
+from discord.ext import commands
 from coinbase.wallet.client import Client
 from dotenv import load_dotenv
 import chartGenerator
